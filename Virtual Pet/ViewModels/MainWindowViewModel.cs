30477c029806cb5d417ec30f8d385dc86c6c5acd
ryangordon11235 <78880773+ryangordon11235@users.noreply.github.com>
2022-06-06 16:03:10 +1200

    Set up GUI with material design.

---
+++ Virtual Pet/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace Virtual_Pet.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Virtual Pet";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
